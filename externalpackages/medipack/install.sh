c450721faa2081392b72fa3237afd6e1de792f5a
adamjhepburn <ajh24@aber.ac.uk>
2025-08-28 08:26:49 +0100

    chore: Untrack and ignore externalpackages

--- externalpackages/medipack/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-#Some cleanup
-rm -rf install
-
-#Download development version
-git clone https://github.com/SciCompKL/MeDiPack.git install
--- externalpackages/topotoolbox/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf install
-svn co https://github.com/wschwanghart/topotoolbox.git/trunk install
