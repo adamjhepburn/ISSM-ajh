8013e8c050f0097324efff175cbe50c3a6c295b0
adamjhepburn <ajh24@aber.ac.uk>
2025-10-08 09:19:02 +0100

    Untrack and ignore externalpackages directory

--- externalpackages/geocode/install.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-unzip geoCode.zip
-sed -i -e 's/http:/https:/g' geoCode.m
--- externalpackages/triangle/configs/javascript/triangle.h.patch.js
+++
@@ -1,8 +0,0 @@
-250a251,257
-> /*Patch for ISSM*/
-> #ifndef REAL
-> typedef double REAL;
-> typedef void VOID;
-> #endif
-> /*End patch*/
-> 
